83f7cace66710f22cbead1f9df8a4f612a317a60
Virenk21 <viren.d222111@gmail.com>
2022-09-02 17:37:59 +0530

    added map

--- src/App.js
+++ src/App.js
@@ -1,12 +1,49 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import { GoogleMap, useJsApiLoader } from "@react-google-maps/api";
 
-function App() {
-  return (
-    <div className="App">
-   WELCOME to Linkedin connection with geo Mapping
-    </div>
+const containerStyle = {
+  width: "1300px",
+  height: "600px",
+};
+
+const center = {
+  lat: 28.7041,
+  lng: 77.1025,
+};
+
+function MyComponent() {
+  const { isLoaded } = useJsApiLoader({
+    id: "google-map-script",
+    googleMapsApiKey: "AIzaSyAl9D_gJluOf2uSZD2KdheX5Z8rAUGyMQA",
+  });
+
+  const [map, setMap] = React.useState(null);
+
+  const onLoad = React.useCallback(function callback(map) {
+    const bounds = new window.google.maps.LatLngBounds(center);
+    map.fitBounds(bounds);
+    setMap(map);
+  }, []);
+
+  const onUnmount = React.useCallback(function callback(map) {
+    setMap(null);
+  }, []);
+
+  return isLoaded ? (
+    <GoogleMap
+      mapContainerStyle={containerStyle}
+      center={center}
+      zoom={7}
+      onLoad={onLoad}
+      onUnmount={onUnmount}
+    >
+      {/* Child components, such as markers, info windows, etc. */}
+      <></>
+    </GoogleMap>
+  ) : (
+    <></>
   );
 }
 
-export default App;
+export default React.memo(MyComponent);
