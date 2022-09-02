5ea596f70c5a0452a159071aab5a1b4e6a80dc27
Virenk21 <viren.d222111@gmail.com>
2022-09-02 15:10:07 +0530

    updated initial files

--- src/App.js
+++ src/App.js
@@ -4,20 +4,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Viren
-        </a>
-      </header>
+   WELCOME to Linkedin connection with geo Mapping
     </div>
   );
 }
